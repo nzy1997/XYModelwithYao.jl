0aa14f94c964225738fd2051cd66adbb386e8269
Documenter.jl <documenter@juliadocs.github.io>
2024-04-01 06:04:06 +0000

    build based on 8f337f6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = XYModelwithYao","category":"page"},{"location":"#XYModelwithYao","page":"Home","title":"XYModelwithYao","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for XYModelwithYao.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [XYModelwithYao]","category":"page"}]
+}
